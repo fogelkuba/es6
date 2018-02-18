5fab4843ad3f0574055f59e9a9afd383e92748a3
fogelkuba <fogelkuba@gmail.com>
2018-02-18 10:29:42 +0100

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,17 @@ module.exports = {
 
   entry: './assets/js/scripts.js',
   output: {filename: './public/js/bundle.js'
-
-
   },
-
-
   module:{
-
       loaders: [
-
-
           {
-
               test: /\.js$/,
               exclude: /node_modules/,
               loader: 'babel-loader',
               query: {
-
                   presets:['es2015']
-
               }
-
-
-
-
           }
-
-
-
       ]
-
-
   }
-
-
-
-
-
 };
