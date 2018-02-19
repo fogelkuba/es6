f7de1bff72259ec85ffe7c95c1004197ecbc0f62
fogelkuba <fogelkuba@gmail.com>
2018-02-19 21:41:29 +0100

    arror func

--- assets/js/arrow.js
+++ assets/js/arrow.js
@@ -2,6 +2,9 @@ console.log('arrow functions');
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
+const calc = (a = 2, b = 3) => {
+    return a + b;
+}
 
-
+console.log(calc());
 /* end */ })
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -87,7 +87,16 @@ console.log('arrow functions');
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  /* end */});
+    var calc = function calc() {
+        var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
+        var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
+
+        return a + b;
+    };
+
+    console.log(calc());
+    /* end */
+});
 
 /***/ }),
 /* 2 */
