863402d72f27152d545aced7046babee667f7404
fogelkuba <fogelkuba@gmail.com>
2018-02-24 17:15:52 +0100

    class 19

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,7 +1,20 @@
-console.log('class');
-
-document.addEventListener('DOMContentLoaded', ()=> {
-
-
-
-/* end */ })
+// console.log('class');
+//
+// document.addEventListener('DOMContentLoaded', ()=> {
+//
+//     class Person {
+//         constructor(name, title){
+//             this.name = name;
+//             this.title = title;
+//         }
+//
+//         displayData(){
+//             console.log(`${this.name} is a great ${this.title}`)
+//         }
+//
+//     }
+//
+//     let john = new Person('John', 'Dentist');
+//     john.displayData();
+//
+// /* end */ })
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -101,13 +101,27 @@ document.addEventListener('DOMContentLoaded', function () {
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+// console.log('class');
+//
+// document.addEventListener('DOMContentLoaded', ()=> {
+//
+//     class Person {
+//         constructor(name, title){
+//             this.name = name;
+//             this.title = title;
+//         }
+//
+//         displayData(){
+//             console.log(`${this.name} is a great ${this.title}`)
+//         }
+//
+//     }
+//
+//     let john = new Person('John', 'Dentist');
+//     john.displayData();
+//
+// /* end */ })
 
-
-console.log('class');
-
-document.addEventListener('DOMContentLoaded', function () {
-
-  /* end */});
 
 /***/ }),
 /* 3 */
